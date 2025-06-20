2469825ffcbeac9c0b50f804e9653a1c61d91bfe
NuryeNigusMekonen <nurye.nigus.me@gmail.com>
2025-06-20 14:48:36 +0300

    modeling

--- src/utils.py
+++
@@ -1,7 +0,0 @@
-def calculate_loss_ratio(df):
-    """Compute the loss ratio = TotalClaims / TotalPremium."""
-    if 'TotalClaims' in df.columns and 'TotalPremium' in df.columns:
-        df['LossRatio'] = df['TotalClaims'] / df['TotalPremium']
-    else:
-        raise KeyError("Required columns missing.")
-    return df
